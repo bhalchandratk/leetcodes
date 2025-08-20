af2550890d6b03e493350293fee012b3823f4919
bhalchandratk <bhalchandratk@gmail.com>
2025-08-20 22:03:21 +0530

    lrucache algorithm complete

--- problems/LRUCache.java
+++ problems/LRUCache.java
@@ -1,18 +1,55 @@
  package problems;
 
 import java.util.HashMap;
-
+import java.time.LocalDateTime;
+import java.util.Comparator;
+/**
+ * LRUCache implementation in Java.
+ * This is a basic implementation that does not yet handle eviction of least recently used items.
+ * It currently only supports put and get operations.
+ */
 public class LRUCache<KEY,VALUE> {
+
+    private static class Node<Value>{
+        private Value value;
+        private LocalDateTime timestamp;
+
+        public Node(Value value) {
+            this.value = value;
+            this.timestamp = LocalDateTime.now();
+        }
+
+        public Value getValue(){
+            // Update the timestamp to mark this node as recently used
+            this.timestamp = LocalDateTime.now();
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" + "value=" + value +  ", timestamp=" + timestamp + "}";
+        }
+    }
+
     private int capacity;
-    private HashMap<KEY,VALUE> storage = new HashMap<>();
+    private HashMap<KEY,Node<VALUE>> storage = new HashMap<>();
 
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        LRUCache<String, String> lruCache = new LRUCache(10);
-        lruCache.put("Bhalchandra", "Kadam");
+        LRUCache<String, String> lruCache = new LRUCache(5);
+        lruCache.put("Lonavala", "50km");
+        lruCache.put("Mulshi", "70km");
+        lruCache.put("Mahabaleshwar", "170km");
+        lruCache.put("Wai", "140km");
+        lruCache.put("Ozar", "180km");
 
         System.out.println(lruCache);
+
+        System.out.println("Get Lonavala: " + lruCache.get("Lonavala"));
+        System.out.println("Get Mulshi: " + lruCache.get("Mahabaleshwar"));
+        lruCache.put("Panchgani", "160km");
+        System.out.println("After adding Panchgani: " + lruCache);
     }
 
     public LRUCache(int capacity){
@@ -20,11 +57,22 @@ public class LRUCache<KEY,VALUE> {
     }
 
     public void put(KEY key, VALUE value){
-        storage.put(key, value);
+        if(storage.size() >= capacity){
+            evit();
+        }
+
+        storage.put(key, new Node<>(value));
+    }
+
+    private void evit() {
+         
+         var evitEntry = storage.entrySet().stream().min(Comparator.comparing(entry -> entry.getValue().timestamp));
+         System.out.println("Evicting: " + evitEntry);
+         storage.remove(evitEntry.get().getKey());
     }
 
     public VALUE get(KEY key){
-        return storage.get(key);
+        return storage.get(key).getValue();
     }
 
     @Override
